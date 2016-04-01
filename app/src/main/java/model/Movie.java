de3c59eb3cbbed9f92b30c48a7859e8e79d212bf
Lucas Corrêa <lup.correa+git@gmail.com>
2016-04-01 10:29:43 +0200

    Model backbone created. Several TODO annotations were made so we can build several layers of the application at once.

---
+++ app/src/main/java/model/Movie.java
@@ -0,0 +1,65 @@
+package model;
+
+/**
+ * Author: lupcorrea
+ * Day: 01/04/16
+ */
+
+/* TODO: Edit the release methods so it only deals with years. */
+/* TODO: Add the lists for actors and producers, if possible. */
+public class Movie {
+    private String name;
+    private String release;
+    private String category;
+    private String popularity;
+    private String country;
+    private String sinopsis;
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ app/src/main/java/model/ProfileModel.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.LinkedList;
+
+/**
+ * Author: lupcorrea
+ * Day: 01/04/16
+ */
+
+/* TODO: Set the api key. */
+/* TODO: Instantiate the profile image. */
+/* TODO: Build the constructor with dynamic data. */
+/* TODO: Add the model access methods on demand. It's useless to build lots of methods now that will not be used in the last version. */
+public class ProfileModel {
+    /* Profile attributes */
+    private String name;
+    private final String api_key = "ff";
+
+    /* Movies lists */
+    private LinkedList<Movie> topList = new LinkedList<>();
+    private LinkedList<Movie> bottomList = new LinkedList<>();
+    private LinkedList<Movie> toDoList = new LinkedList<>();
+
+    /* Constructor */
+    public ProfileModel() {
+
+    }
+}
